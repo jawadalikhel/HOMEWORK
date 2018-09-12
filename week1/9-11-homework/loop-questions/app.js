b05ebcb77b5b0b12d923dbdb99514014d668745e
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-12 10:26:52 -0500

    Commit 9- Created loops.js file

---
+++ week1/9-11-homework/loop-questions/app.js
@@ -0,0 +1,96 @@
+/*
+// 1.
+let num = 0;
+while(num <= 99)
+{
+	console.log(num);
+	num++;
+}
+
+
+// 2.
+let num = 99;
+while(num >=0)
+{
+	console.log(num);
+	--num;
+}
+
+
+// 3.
+
+let num = 298;
+while(num >= 1)
+{
+	console.log(num);
+	num -= 3;
+}
+
+
+for(let i= 98; i >=1; i++)
+{
+	console.log(i);
+	i += 2;
+}
+
+//4.
+let i = 0;
+while(i <= 98)
+{
+	console.log(i);
+	i += 2;
+}
+
+
+//5. 
+
+for (let i = 0; i <= 98; i++)
+{
+	if (i % 2 == 0)
+	{
+		console.log(i)
+	}
+}
+
+// 6.
+for (let i=98; i >= 0; i--)
+{
+	if(i % 2 != 0)
+	{
+		console.log(i);
+	}
+}
+
+
+
+for(let i = 49; i <= 72; i++)
+{
+	console.log(i);
+}
+
+
+
+for(let i = 81; i >= 26; i--)
+{
+	console.log(i);
+}
+
+*/
+
+let num = 1;
+while(num <= 91)
+{
+	console.log(num);
+	num *= 3;
+}
+
+
+
+
+
+
+
+
+
+
+
