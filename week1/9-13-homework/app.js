9aedd556ce9f6d3b45ba80d5b75ffac42ee2b854
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-13 09:45:51 -0500

    commit 3- Excited Kittens answered

--- week1/9-13-homework/app.js
+++ week1/9-13-homework/app.js
@@ -5,12 +5,30 @@
 // 	console.log(i);
 // }
 
+
 //// Get Even ////
 
-for(let i = 0; i <= 200; i++)
-{
-	if(i % 2 === 0)
-	{
-		console.log(i);
-	}
-}
+// for(let i = 0; i <= 200; i++)
+// {
+// 	if(i % 2 === 0)
+// 	{
+// 		console.log(i);
+// 	}
+// }
+
+
+////// Excited Kitten ///////
+
+// const array = [ "...human...why you taking pictures of me?...", 
+// 				"...the catnip made me do it...", 
+// 				"...why does the red dot always get away..."];
+
+// for(let i = 1; i <= 20; i++)
+// {
+// 	console.log('Love me, pet me! HSSSSSS!');
+
+// 	if(i % 2 === 0)
+// 	{
+// 		console.log(array[Math.floor(Math.random()*array.length)]);
+// 	}
+// }
