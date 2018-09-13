cc879f03133746e9f45f3510c1b0ed311c19d439
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-13 09:29:26 -0500

    Commit 2- Get Even answered

--- week1/9-13-homework/app.js
+++ week1/9-13-homework/app.js
@@ -1,6 +1,16 @@
 //// Easy Going /////
 
-for(let i = 1; i <= 20; i++)
+// for(let i = 1; i <= 20; i++)
+// {
+// 	console.log(i);
+// }
+
+//// Get Even ////
+
+for(let i = 0; i <= 200; i++)
 {
-	console.log(i);
+	if(i % 2 === 0)
+	{
+		console.log(i);
+	}
 }
