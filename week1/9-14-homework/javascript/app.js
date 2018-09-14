8b9afd625e409b2eb9513369f5c7a1aeafe8f910
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-13 23:46:29 -0500

    Commit 2- Palindrome

--- week1/9-14-homework/javascript/app.js
+++ week1/9-14-homework/javascript/app.js
@@ -1,12 +1,48 @@
 //// 1. Verbal questions
-What is the difference between a parameter and an argument?
-- parameters is the variable in the declatation of function
-- arguments are the data we data we are passing into parameters
+// What is the difference between a parameter and an argument?
+// - parameters is the variable in the declatation of function
+// - arguments are the data we data we are passing into parameters
 
-Within a function, what is the difference between return and console.log?
--console.log will write the argument from our funtion in the console
-- return is used to just get the outcome of something
+// Within a function, what is the difference between return and console.log?
+// -console.log will write the argument from our funtion in the console
+// - return is used to just get the outcome of something
 
-What are the implications of the ability of a function to return a value?
--return by value
--return by reference
+// What are the implications of the ability of a function to return a value?
+// -return by value
+// -return by reference
+
+///////////////
+
+//// 2. Palindrome again
+
+// const checkPalindrome = (string) =>
+// {
+// 	if(string === string.split("").reverse("").join("")){
+// 		return true;
+// 	}
+// 	else{
+// 		return false;
+// 	}
+// }
+
+// console.log(checkPalindrome('jawaj'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
