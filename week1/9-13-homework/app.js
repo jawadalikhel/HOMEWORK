fd63835397093a2dc5246d609de5371c3fbe0226
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-13 23:02:06 -0500

    Commit 1- verbal questions

--- week1/9-13-homework/app.js
+++ week1/9-13-homework/app.js
@@ -32,3 +32,5 @@
 // 		console.log(array[Math.floor(Math.random()*array.length)]);
 // 	}
 // }
+
+
---
+++ week1/9-14-homework/javascript/app.js
@@ -0,0 +1,12 @@
+//// 1. Verbal questions
+What is the difference between a parameter and an argument?
+- parameters is the variable in the declatation of function
+- arguments are the data we data we are passing into parameters
+
+Within a function, what is the difference between return and console.log?
+-console.log will write the argument from our funtion in the console
+- return is used to just get the outcome of something
+
+What are the implications of the ability of a function to return a value?
+-return by value
+-return by reference
