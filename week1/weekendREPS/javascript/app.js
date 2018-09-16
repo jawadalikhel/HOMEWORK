5dd60e594d5a37cf60fb2308855727a0b2b18fff
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-16 16:36:39 -0500

    Commit A- Q+A

--- week1/weekendREPS/javascript/app.js
+++ week1/weekendREPS/javascript/app.js
@@ -1,1 +1,24 @@
 console.log('js is working here!');
+
+// I. Variables & Datatypes
+//  A. Q + A
+
+// 1. How do we assign a value to a variable?
+// 	- let x = 5;
+// 2. How do we change the value of a variable?
+// 	ex: let myName = 'jawad'
+// 		console.log(myName) ---> jawad
+// 		myName = 'sahil'
+// 		console.log(myName) ---> sahil
+
+// 3. How do we assign an existing variable to a new variable?
+// 4. Remind me, what are declare, assign, and define?
+// 	-declaration is a statement that describes an identifier, such as the name of a variable or a function.
+// 	-assign is when you assign a value to a varialbe 
+// 	-
+// 5. What is pseudocoding and why should you do it?
+// 	- is writing down the logic for a solution to a specific coding
+// 	  challenge using plain English.
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// 	- planning : 75-90%
+// 	- typing code: 10-25%
