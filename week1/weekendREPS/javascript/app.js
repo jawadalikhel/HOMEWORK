88678bf8de166498b2e5eeff845d9cc6c8305ab8
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-16 17:02:13 -0500

    Commit D. The farm

--- week1/weekendREPS/javascript/app.js
+++ week1/weekendREPS/javascript/app.js
@@ -43,24 +43,37 @@ console.log('js is working here!');
 
 ///////////////////////////////////////////////////
 
-// C. Booleans
+// // C. Booleans
 
-  const a = 4;
-  const b = 53;
-  const c = 57;
-  const d = 16;
-  const e = 'Kevin';
+//   const a = 4;
+//   const b = 53;
+//   const c = 57;
+//   const d = 16;
+//   const e = 'Kevin';
 
-  console.log(a, b);
-  console.log(c, d);
-  console.log('Name', 'Name');
-  console.log(true, false);
-  console.log(false, false, false, false, false, false, true);
-  console.log(false, false)
-  console.log(e, 'Kevin');
-  console.log(a, b, c);
-  console.log(a, a, d);
-  console.log(48, '48');
+//   console.log(a, b);
+//   console.log(c, d);
+//   console.log('Name', 'Name');
+//   console.log(true, false);
+//   console.log(false, false, false, false, false, false, true);
+//   console.log(false, false)
+//   console.log(e, 'Kevin');
+//   console.log(a, b, c);
+//   console.log(a, a, d);
+//   console.log(48, '48');
+
+/////////////////////////////////////////////
+//// D. Farm
+
+let animal = 'cow';
+if(animal === 'cow')
+{
+	console.log('mooooooo');
+}
+
+else{
+	console.log('Hey! You\'re not a cow');
+}
 
 
 
