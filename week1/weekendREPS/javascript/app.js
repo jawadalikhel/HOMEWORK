326109e2df317b625277fba52bd54f397563c92e
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-16 16:51:35 -0500

    Commit C. Booleans

--- week1/weekendREPS/javascript/app.js
+++ week1/weekendREPS/javascript/app.js
@@ -25,21 +25,42 @@ console.log('js is working here!');
 
 //////////////////////////////////////////////
 
-// B. Strings
-// 1.
-let firstVariable = 'Hello World';
-firstVariable = 10;
-console.log(firstVariable);
+// // B. Strings
+// // 1.
+// let firstVariable = 'Hello World';
+// firstVariable = 10;
+// console.log(firstVariable);
 
-secondVariable = firstVariable;
-console.log(secondVariable);
+// secondVariable = firstVariable;
+// console.log(secondVariable);
 
-secondVariable = 'Changing the secondVariable to this string';
-console.log(secondVariable);
+// secondVariable = 'Changing the secondVariable to this string';
+// console.log(secondVariable);
 
-// 2. 
-let yourName = 'Jawad Alikhel';
-console.log('Hello, my name is ' + yourName);
+// // 2. 
+// let yourName = 'Jawad Alikhel';
+// console.log('Hello, my name is ' + yourName);
+
+///////////////////////////////////////////////////
+
+// C. Booleans
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a, b);
+  console.log(c, d);
+  console.log('Name', 'Name');
+  console.log(true, false);
+  console.log(false, false, false, false, false, false, true);
+  console.log(false, false)
+  console.log(e, 'Kevin');
+  console.log(a, b, c);
+  console.log(a, a, d);
+  console.log(48, '48');
 
 
 
