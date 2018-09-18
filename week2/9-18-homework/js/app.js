c2264a26fac059c7372259f20b39812c5399cb2a
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-18 09:26:34 -0500

    Commit A. Data types

---
+++ week2/9-18-homework/js/app.js
@@ -0,0 +1,22 @@
+console.log('js is running');
+// 1. Data types
+A light switch that can be either on or off.
+- a Booleans
+	-if(if light === true){
+		console.log('the light is on')
+	}
+	else(
+		console.log('the light is off')
+		)
+
+A user's email address.
+	-use string 
+	-person.email = "abcd@yahoo.com";
+A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+	-use string
+A list of student names from our class.
+	--string
+A list of student names from our class, each with a location.
+	-string 
+A list of student names from our class, each with a location and each with a list of favorite tv shows.
+	-strings
