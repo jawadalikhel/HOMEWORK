159870e69585e7be508ffe0925ed345a3caf7a21
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-18 09:35:26 -0500

    Commit- Data Structures

--- week2/9-18-homework/js/app.js
+++ week2/9-18-homework/js/app.js
@@ -1,22 +1,40 @@
 console.log('js is running');
-// 1. Data types
-A light switch that can be either on or off.
-- a Booleans
-	-if(if light === true){
-		console.log('the light is on')
-	}
-	else(
-		console.log('the light is off')
-		)
+// // 1. Data types
+// A light switch that can be either on or off.
+// - a Booleans
+// 	-if(if light === true){
+// 		console.log('the light is on')
+// 	}
+// 	else(
+// 		console.log('the light is off')
+// 		)
 
-A user's email address.
-	-use string 
-	-person.email = "abcd@yahoo.com";
-A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-	-use string
-A list of student names from our class.
-	--string
-A list of student names from our class, each with a location.
-	-string 
-A list of student names from our class, each with a location and each with a list of favorite tv shows.
-	-strings
+// A user's email address.
+// 	-use string 
+// 	-person.email = "abcd@yahoo.com";
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// 	-use string
+// A list of student names from our class.
+// 	--string
+// A list of student names from our class, each with a location.
+// 	-string 
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// 	-strings
+
+///////////////////////////////////////
+
+// 2. Take it Easy
+// 1.
+const array = ['Red','Orange','Yellow','Green','Blue','Indigo','Violet'];
+//2. 
+console.log(array[4]);
+//3.
+const jawad = {
+	name: 'Jawad Alikhel',
+	favoriteFood: 'Burgers',
+	hobby: 'Photography',
+	currentTown: 'Austin',
+	favDataType: 'array'
+}
+//4.
+console.log(jawad.hobby);
