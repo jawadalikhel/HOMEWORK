1d39f77b3e1f54ee48e8bbb6bf6eec233af61bbc
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-18 09:59:48 -0500

    commit: crazyObjects

--- week2/9-18-homework/js/app.js
+++ week2/9-18-homework/js/app.js
@@ -23,18 +23,104 @@ console.log('js is running');
 
 ///////////////////////////////////////
 
-// 2. Take it Easy
-// 1.
-const array = ['Red','Orange','Yellow','Green','Blue','Indigo','Violet'];
-//2. 
-console.log(array[4]);
-//3.
-const jawad = {
-	name: 'Jawad Alikhel',
-	favoriteFood: 'Burgers',
-	hobby: 'Photography',
-	currentTown: 'Austin',
-	favDataType: 'array'
+// // 2. Take it Easy
+// // 1.
+// const array = ['Red','Orange','Yellow','Green','Blue','Indigo','Violet'];
+// //2. 
+// console.log(array[4]);
+// //3.
+// const jawad = {
+// 	name: 'Jawad Alikhel',
+// 	favoriteFood: 'Burgers',
+// 	hobby: 'Photography',
+// 	currentTown: 'Austin',
+// 	favDataType: 'array'
+// }
+// //4.
+// console.log(jawad.hobby);
+
+///////////////////////////////////////
+
+// 3. Crazy Object!
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
 }
+//1.
+console.log(crazyObject.taco[1].salsa[5]);
+//2. 
+console.log(crazyObject.larry.quotes[0]);
+//3.
+console.log(crazyObject.larry.characters[2].favourtieHobby);
 //4.
-console.log(jawad.hobby);
+console.log(crazyObject.larry.nicknames[1]);
+//5.
+console.log(crazyObject.larry.characters[1].name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
