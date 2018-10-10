743da3bee74f2714ef2f2c9da1167b0117924e3b
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-10-10 09:25:14 -0500

    Commit 2- Connected my database, can see json in the browser

--- october-HW/10-10-poke-express/server.js
+++ october-HW/10-10-poke-express/server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 
+const Pokemon = require('./module/pokemon');
+
+app.get('/', (req, res) =>{
+	res.send('hey hey heyyy');
+})
+
+app.get('/pokemon', (req, res) =>{
+	res.send(Pokemon);
+})
+
+app.get('/pokemon/:index',(req, res) =>{
+	res.render('show.ejs', {
+		poke: Pokemon[req.params.index]
+	});
+});
+
+
 
 app.listen(3000, () =>{
 	console.log('im alive and youu!!');
