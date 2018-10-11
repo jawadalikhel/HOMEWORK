c90de06ff5c586f10e5bcf6c7bc2740a634ce8c2
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-10-11 12:14:11 -0500

    created the info button

--- october-HW/10-10-poke-express/server.js
+++ october-HW/10-10-poke-express/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const Pokemon = require('./module/pokemon');
 
-const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) =>{
 	res.send('hey hey heyyy');
@@ -13,38 +17,48 @@ app.get('/', (req, res) =>{
 // });
 
 
+// app.get('/pokemon', (req, res) =>{
+// 	res.render('index.ejs', {
+// 		pokemon: Pokemon
+// 	});
+// });
+
 app.get('/pokemon', (req, res) =>{
 	res.render('index.ejs', {
 		pokemon: Pokemon
 	});
 });
 
-
 app.get('/pokemon/:index', (req, res) =>{
 	res.render('show.ejs', {
 		pokemon: Pokemon[req.params.index]
-	})
+	});
 
-});
-
-// app.get('/pokemon/:id/', (req, res) =>{
-// 	res.render('show.ejs', {
-// 		showPoke: 
-
-// 	});
-// });
+})
 
 
+///// add a new pokemon
+app.get('/pokemon/new' ,(req, res) =>{
+	res.render('new.ejs');
+})
 
 
+app.post('/pokemon/:index', (req, res) =>{
+	console.log(req.body, ' this is where our info from the pokemon form will live');
+
+	Pokemon.push(req.body);
+	res.redirect('/pokemon');
+})
+
+/// edit pokemon
+app.get('/pokemon/:id/edit', (req, res) =>{
+  res.render('edit.ejs', {
+    pokemon: Pokemon[req.params.id],
+    id: req.params.id
+  })
 
 
-
-
-
-
-
-
+})
 
 
 app.listen(3000, () =>{
